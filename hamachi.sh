462f89d7352e91af1192e14bcdec50140a8edbbd
Yeferal <yefercuber20@gmail.com>
2021-05-05 00:07:06 -0600

    se agrega bash para descargar hamachi

---
+++ hamachi.sh
@@ -0,0 +1,5 @@
+echo 'wget http://www.vpn.net/installers/logmein-hamachi-2.1.0.165-1.x86_4.rpm';
+wget http://www.vpn.net/installers/logmein-hamachi-2.1.0.165-1.x86_4.rpm 
+
+echo 'yum install logmein-hamachi-2.1.0.165-1.x86_4.rpm';
+yum install logmein-hamachi-2.1.0.165-1.x86_4.rpm
