c4c46ad0d8a0061e0dff47f4418efb21d7245530
Yeferal <yefercuber20@gmail.com>
2021-05-05 22:28:20 -0600

    se agrega el conect_hamachi.sh y algunos comando de ayuda

--- hamachi.sh
+++ hamachi.sh
@@ -1,5 +1,5 @@
-echo 'wget http://www.vpn.net/installers/logmein-hamachi-2.1.0.165-1.x86_4.rpm';
-wget http://www.vpn.net/installers/logmein-hamachi-2.1.0.165-1.x86_4.rpm 
+echo 'wget http://www.vpn.net/installers/logmein-hamachi-2.1.0.203-1.x86_64.rpm';
+wget http://www.vpn.net/installers/logmein-hamachi-2.1.0.203-1.x86_64.rpm 
 
-echo 'yum install logmein-hamachi-2.1.0.165-1.x86_4.rpm';
-yum install logmein-hamachi-2.1.0.165-1.x86_4.rpm
+echo 'yum install logmein-hamachi-2.1.0.203-1.x86_64.rpm';
+yum install logmein-hamachi-2.1.0.203-1.x86_64.rpm	
